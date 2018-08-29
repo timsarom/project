5be3c3a8caec8cc121ad8ea4f5b736de13ded07c
Romans Timsans <romans.timsans@tieto.com>
2018-08-29 09:50:38 +0300

    finished chapter 7

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,9 @@
 class Product < ApplicationRecord
+	validates :title, :description, :image_url, presence: { message: 'field must be filled with valid data'}
+	validates :price, numericality: { greater_than_or_equal_to: 0.01, message: "field must contain number, which ir larger than 0.01" }
+	validates :title, uniqueness: true, length: {minimum: 10, message: 'field must contain at least 10 characters'}
+	validates :image_url, allow_blank: true, format: {
+		with: %r{\.(gif|jpg|png)\Z}i,
+		message: 'must be a URL for GIF, JPG or PNG image.'
+	}
 end
