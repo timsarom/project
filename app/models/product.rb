4bef7bdfd7714f3421cf34840c89af126060d3d9
Romans Timsans <romans.timsans@tieto.com>
2018-09-04 14:34:06 +0300

    finished chapter 12

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,4 +1,5 @@
 class Product < ApplicationRecord
+	include ActiveModel::Serializers::Xml
 	has_many :line_items
 	has_many :orders, through: :line_items
 	before_destroy :ensure_not_referenced_by_any_line_item
