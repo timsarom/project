ce380ad8256556ea20d0cb5f7299ca915afab0d6
Romans Timsans <r.timsans@gmail.com>
2018-11-29 12:32:36 +0200

    changes to deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -31,7 +31,7 @@ role :db, domain
 append :linked_files, "config/database.yml", "config/secrets.yml"
 
 # Default value for linked_dirs is []
-append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "vendor/bundle", "public/assets"
+# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system", "vendor/bundle", "public/assets"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
